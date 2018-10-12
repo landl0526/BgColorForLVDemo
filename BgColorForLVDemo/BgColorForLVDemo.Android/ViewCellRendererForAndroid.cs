1ee7fc3d8cb894168b5cc9e03969a82ec00a5bd9
Land Lu <v-luting@microsoft.com>
2018-10-12 10:15:24 +0800

    Add project files.

---
+++ BgColorForLVDemo/BgColorForLVDemo.Android/ViewCellRendererForAndroid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BgColorForLVDemo;
+using BgColorForLVDemo.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(MyViewCell), typeof(ViewCellRendererForAndroid))]
+namespace BgColorForLVDemo.Droid
+{
+    public class ViewCellRendererForAndroid : ViewCellRenderer
+    {
+        private Android.Views.View _cellCore;
+        private bool _selected = false;
+
+        protected override Android.Views.View GetCellCore(Cell item, Android.Views.View convertView, ViewGroup parent, Context context)
+        {
+            _cellCore = base.GetCellCore(item, convertView, parent, context);
+            return _cellCore;
+        }
+
+        protected override void OnCellPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnCellPropertyChanged(sender, args);
+            if (args.PropertyName == "IsSelected")
+            {
+                _selected = !_selected;
+                var extendedViewCell = sender as ViewCell;
+                if (_selected)
+                    _cellCore.SetBackgroundColor(Android.Graphics.Color.Red);
+                else
+                    _cellCore.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ BgColorForLVDemo/BgColorForLVDemo.iOS/ViewCellRendererForiOS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BgColorForLVDemo;
+using BgColorForLVDemo.iOS;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(MyViewCell), typeof(ViewCellRendererForiOS))]
+namespace BgColorForLVDemo.iOS
+{
+    public class ViewCellRendererForiOS : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            cell.SelectedBackgroundView = new UIView { BackgroundColor = UIColor.Red };
+
+            return cell;
+        }
+    }
+}
